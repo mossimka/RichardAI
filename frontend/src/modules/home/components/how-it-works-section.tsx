d9f59132f917560908fd4238b65df8979b00f3af
mossimka <maksimsarsekeev@gmail.com>
2025-08-07 11:06:29 +0500

    files init

---
+++ frontend/src/modules/home/components/how-it-works-section.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HowItWorksSection = () => {
+  return (
+    <div>how-it-works.section</div>
+  )
+}
+
+export default HowItWorksSection
---
+++ frontend/src/modules/user/components/user-info.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserInfo = () => {
+  return (
+    <div>user-info</div>
+  )
+}
+
+export default UserInfo
