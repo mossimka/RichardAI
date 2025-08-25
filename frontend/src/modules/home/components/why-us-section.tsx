1510156dfb171c419826b2bf62cd33884025d327
Daniyar <daniyarmunsyzbaev@gmail.com>
2025-08-25 21:40:03 +0500

    Made finn images lightweight

--- frontend/src/modules/home/components/why-us-section.tsx
+++ frontend/src/modules/home/components/why-us-section.tsx
@@ -71,6 +71,7 @@ export default function WhyUsSection() {
               <Image
                 src={finnImages[currentImageIndex]}
                 alt="Finn character"
+                priority
                 width={0}
                 height={0}
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
